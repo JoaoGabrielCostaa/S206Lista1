44ec52820adb276f6f1f7ae4cb927f03e0cfbfe2
Daniela Faria <100132170+01danifaria@users.noreply.github.com>
2025-05-27 17:42:39 -0300

    Update Lista1_LaraCG_Projetos.cy.js
    Adicionando mais testes de projetos

--- cypress/e2e/Lista1_LaraCG_Projetos.cy.js
+++ cypress/e2e/Lista1_LaraCG_Projetos.cy.js
@@ -116,5 +116,66 @@ describe('6 Testes de Projetos', () => {
     //Texto que deve aparecer quando o botão de Salvar Projeto for pressionado
     cy.get('.Toastify__toast-body').should('contain.text', 'Projeto atualizado com sucesso!')
   })
+})
 
+  describe('Testes adicionais', () => {
+  it('Teste 7: Modificar título do projeto #12 e salvar com sucesso', () => {
+    logarNoSite()
+    cy.get('[href="/adm/projetos/12"]').click()
+    cy.get('.sc-iCKXBC > [viewBox="0 0 576 512"]').click()
+    cy.get('input[name="titulo"]').clear().type('Título Automatizado')
+    cy.get('.sc-iCKXBC > [viewBox="0 0 448 512"]').click()
+    cy.get('.iTLMzn > .sc-csKJxZ').click()
+    cy.get('.Toastify__toast-body').should('contain.text', 'Projeto atualizado com sucesso!')
+  })
+
+  it('Teste 8: Adicionar descrição no projeto #12 e salvar', () => {
+    logarNoSite()
+    cy.get('[href="/adm/projetos/12"]').click()
+    cy.get('textarea[name="descricao"]').clear().type('Descrição inserida via teste automatizado.')
+    cy.get('.iTLMzn > .sc-csKJxZ').click()
+    cy.get('.Toastify__toast-body').should('contain.text', 'Projeto atualizado com sucesso!')
+  })
+
+  it('Teste 9: Alterar status e paralela juntos no projeto #12 e salvar', () => {
+    logarNoSite()
+    cy.get('[href="/adm/projetos/12"]').click()
+    cy.get('.sc-gUjWJS>.sc-kiTBBF>path').click()
+    cy.get(':nth-child(1) > .sc-ldgOGP').select('Sem pendências')
+    cy.get(':nth-child(2) > .sc-ldgOGP').select('Projeto teste')
+    cy.get('.sc-hiTDLB>path').click()
+    cy.get('.iTLMzn > .sc-csKJxZ').click()
+    cy.get('.Toastify__toast-body').should('contain.text', 'Projeto atualizado com sucesso!')
+  })
+
+  it('Teste 10: Tentar salvar projeto #8 com título vazio', () => {
+    logarNoSite()
+    cy.get('[href="/adm/projetos/8"]').click()
+    cy.get('.sc-iCKXBC > [viewBox="0 0 576 512"]').click()
+    cy.get('input[name="titulo"]').clear()
+    cy.get('.sc-iCKXBC > [viewBox="0 0 448 512"]').click()
+    cy.get('.iTLMzn > .sc-csKJxZ').click()
+    cy.get('.Toastify__toast-body').should('contain.text', 'O nome do projeto não pode estar vazio.')
+  })
+
+  it('Teste 11: Tentar salvar projeto #24 com todos os integrantes removidos', () => {
+    logarNoSite()
+    cy.get('[href="/adm/projetos/24"]').click()
+    cy.get(':nth-child(2)>.sc-jiaSqj>.sc-hVcFVo>path').click()
+    cy.get('.sc-bbxCgr > :nth-child(1)').click()
+    cy.get(':nth-child(1)>.sc-jiaSqj>.sc-hVcFVo>path').click()
+    cy.get('.sc-bbxCgr > :nth-child(1)').click()
+    cy.get('.iTLMzn > .sc-csKJxZ').click()
+    cy.get('.Toastify__toast-body').should('contain.text', 'Erro ao atualizar o projeto.')
+  })
+
+  it('Teste 12: Falha ao alterar status com valor inválido no projeto #12', () => {
+    logarNoSite()
+    cy.get('[href="/adm/projetos/12"]').click()
+    cy.get('.sc-gUjWJS>.sc-kiTBBF>path').click()
+    cy.get(':nth-child(1) > .sc-ldgOGP').select('---')
+    cy.get('.sc-hiTDLB>path').click()
+    cy.get('.iTLMzn > .sc-csKJxZ').click()
+    cy.get('.Toastify__toast-body').should('contain.text', 'Erro ao atualizar o projeto.')
+  })
 })
